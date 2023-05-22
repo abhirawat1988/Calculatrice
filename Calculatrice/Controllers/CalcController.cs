e3ef62e54b221888617823901e02aa4e232cf1d0
RAWAT Abhishek - m061950 <abhishek.rawat@soprasteria.com>
2023-05-22 16:25:15 +0530

    Add all files

---
+++ Calculatrice/Controllers/CalcController.cs
@@ -0,0 +1,47 @@
+﻿using Calculatrice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculatrice.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View(new calc());
+        }
+        [HttpPost]
+        public ActionResult Index(calc c, string calculate)
+        {
+            if(calculate == "add")
+            {
+                c.result = c.number_1 + c.number_2;
+            }
+            else if (calculate == "min")
+            {
+                c.result = c.number_1 - c.number_2;
+            }
+            else if (calculate == "sub")
+            {
+                c.result = c.number_1 * c.number_2;
+            }
+            else
+            {
+                if (c.number_2 != 0)
+                {
+                    c.result = c.number_1 / c.number_2;
+                }
+                else
+                {
+                    c.result = 1234;
+                }
+            }
+            return View(c);
+
+        }
+    }
+}
---
+++ Calculatrice/Models/Calc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculatrice.Models
+{
+    public class calc
+    {
+        public float number_1 { get; set; }
+        public float number_2 { get; set; }
+        public float result { get; set; }
+
+    }
+}
